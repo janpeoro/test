8f3f8ddf49c0290eb0e165997dd13212b2a529fb
Jan Raphael Peoro <japeoro@gbox.adnu.edu.ph>
2024-08-21 10:53:14 +0800

    test

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GGclass</title>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap" rel="stylesheet">
+
+    <!--CSS-->
+    <link rel="stylesheet" href="{{ asset('css/general.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/header.css') }}">
+    
+    </head>
+
+        <body>
+
+            <div class="header">
+                <div class="left-section">
+                    <img class="logo-img" src="{{ asset('img/logo.png') }}" alt="GGclass Logo">
+                    <h1 class="logo-font">GGclass</h1>
+
+                </div>
+
+                <div class="right-section"> 
+                    <img class="create-img" src="{{ asset('img/plus.png') }}" alt="Create">
+
+                    <img class="profile-img" src="{{ asset('img/ainz.jpg') }}" alt="Create">
+
+                </div>
+            </div>
+            
+        </body>
+    </html>
